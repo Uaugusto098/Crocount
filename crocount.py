5ba25a693c6c1401e5e04e56099028dca9a8ca4d
Pedro augusto <pedroaugusto.borges36@gmail.com>
2025-07-28 17:51:40 -0300

    Sistema de horas em tempo real e novos players para serem reproduzidos com novos botoes foram adicionados(problema de fixação do player no  programa resolvido e problema de otimização tambem resolvido a partir da biblioteca multiprocess)

--- crocount.py
+++ crocount.py
@@ -4,9 +4,16 @@ from PIL import Image,ImageTk
 import webview
 from tkhtmlview import HTMLLabel
 import threading
-
+import multiprocessing
+from time import strftime
 #funções
 count=0 
+def time():
+      now=strftime('%H:%M:%S')
+      labeltime.config(text=f'Horas: {now}')
+      labeltime.after(1000,time)
+      
+
 def botaoremove():
         global count
         count-=1
@@ -23,13 +30,32 @@ def botaoadd():
        global botao2
        botao2.config(text=str(count))
 
-     
-def webb():  
-    url="https://www.youtube.com/watch?v=hrJCIFmu8Yc"
-    webview.create_window('Player Mor',url)
+def webb(): 
+    url="https://www.youtube.com/"
+    webview.create_window('Player Mor',url,on_top=True)
     webview.start()
-    threading.Thread(target=webb).start()
-    screen.mainloop()
+    
+def insta():
+      url='https://www.instagram.com/'
+      webview.create_window('Insta',url,on_top=True)
+      webview.start()
+
+def netflix():
+      url='https://www.netflix.com/browse'
+      webview.create_window('Netflix',url,on_top=True)
+      webview.start()
+    
+def process():
+       multiprocessing.Process(target=webb).start()
+
+def processinsta():
+      multiprocessing.Process(target=insta).start()
+
+def processnetflix():
+      multiprocessing.Process(target=netflix).start()
+
+
+
 
 #screen
 screen = tk.Tk()
@@ -49,28 +75,57 @@ imglabel.place(x=0,y=0,relwidth=1,relheight=1)
 
 #widgets
 frame_botoes = tk.Frame(screen)
-frame_botoes.pack(side=tk.BOTTOM, pady=90)
+frame_botoes.pack(side=tk.BOTTOM, pady=100)
 frame_botoes.config(bg='PINK')
+
+frame_time=tk.Frame(screen)
+frame_time.pack(side=tk.LEFT,pady=100)
+
+
+labeltime=tk.Label(screen,bg='PINK', font=("Sweet Apricot",20,'bold'))
+labeltime.place(relx=0.0, rely=1.0, anchor='sw')
+time()
+
 
 
 botao1 = tk.Button(frame_botoes, text="+",bg='PINK', font=("Sweet Apricot",14,'bold'),command=botaoadd)
-botao2 = tk.Label(frame_botoes, text='0',bg='PINK',font=("Sweet Apricot",14,'bold'))
+linhas=tk.Label(frame_botoes,text='Linhas de croche               ',bg='PINK',font=("Sweet Apricot",14,'bold'))
+botao2 = tk.Label(frame_botoes, text='0',bg='PINK',font=("Sweet Apricot",20,'bold'))
 botao3 = tk.Button(frame_botoes, text="-",bg='PINK', font=("Sweet Apricot",14,'bold'),command=botaoremove )
-botao4= tk.Button(frame_botoes,text='Player',bg='PINK', font=("Sweet Apricot",14,'bold'),command=webb)
+botao4= tk.Button(frame_botoes,text='Youtube',bg='PINK', font=("Sweet Apricot",14,'bold'),command=process)
+botao5=tk.Button(frame_botoes,text='instagram',bg='PINK', font=("Sweet Apricot",14,'bold'),command=processinsta)
+botao6=tk.Button(frame_botoes,text='Netflix',bg='PINK', font=("Sweet Apricot",14,'bold'),command=processnetflix)
 
-
+linhas.pack(side=tk.BOTTOM, pady=10)
 botao1.pack(side=tk.LEFT, padx=100)
 botao2.pack(side=tk.LEFT, padx=100)
 botao3.pack(side=tk.LEFT, padx=100)
 botao4.pack()
+botao5.pack()
+botao6.pack()
 
 
 
-frame_player=tk.Frame(screen,bg='BLACK')
-frame_player.place(relx=0.5,rely=0.5,anchor='center')
+
+
 
 #finally
-screen.mainloop()
+
+
+      
+    
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    screen.mainloop()
+
+
+
+
+
+
+
+
 
 
 
